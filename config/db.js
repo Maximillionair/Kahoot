88ef6a6c6728254c439a3d3f5d81db3ea1436ce7
Maximillionair <maxmillian.heim@outlook.com>
2025-05-05 10:28:37 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://10.12.10.99:27017/kahoot-clone');
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    await mongoose.connect(process.env.MONGODB_URI || 'mongodb://10.12.10.99:27017/kahoot-clone');
+    console.log(`MongoDB Connected succesfully`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
